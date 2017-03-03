e3dcf24ebf58e0adf759020ac0a827080bec6278
Oliver Walters <oliver.henry.walters@gmail.com>
2017-03-03 15:32:42 +1100

    Fixes for travis script

--- admin/pretty/add_travis_files.bat
+++ admin/pretty/add_travis_files.bat
@@ -5,9 +5,10 @@ for /d %%i in (*.pretty) do (
     copy .travis.yml %%i
     copy library-check.sh %%i
     cd %%i
+    git pull
     git add .travis.yml
     git add library-check.sh
-    git commit -m "Added travis CI scripts"
+    git commit -m "Fixed travis CI scripts (missing quotes)"
     git push
     cd ..
 )
